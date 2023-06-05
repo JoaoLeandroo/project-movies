95057326fce6fc9dd9d54f3a25da859889ec2880
joaoleandro <new.joaoleandro@gmail.com>
2023-06-05 17:00:23 -0300

    Pagina filme criada

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,8 +1,77 @@
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import api from "../../services/api"
+import '../../index.css'
+import '../Filme/filme.css'
 
 export default function Filme() {
+    const { id } = useParams()
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+    const navigation = useNavigate()
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: 'da359e5e2f2da6874a02ea83beb3b3cf',
+                    language: 'pt-BR',
+                }
+            })
+            .then((response) => {
+                setFilme(response.data)
+                setLoading(false)
+            })
+            .catch(() => {
+                console.log('filme nao encontrado')
+                navigation('/', { replace: true })
+                return
+            })
+        }
+
+        loadFilme()
+
+        return () => {
+            
+        }
+
+    }, [navigation, id])
+
+    function saveFilme() {
+        const minhaLista = localStorage.getItem("@primeflix")
+        
+        let filmesSalvos = JSON.parse(minhaLista) || []
+
+        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+
+        if(hasFilme) {
+            alert("ESSE FILME JA ESTA NA LISTA")
+            return
+        }
+        
+        filmesSalvos.push(filme)
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+        alert("Filme salvo com sucesso!")
+    }
+
+
+    if(loading) {
+        return(
+            <span className="loader"></span>
+        )
+    }
+    
     return(
-        <div>
-            meus filmes
+        <div className="selected-movie" key={filme.id}>
+        <h2>{filme.title}</h2>
+        <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+        <p>
+            {filme.overview}
+        </p>
+        <div className="area-buttons">
+            <button onClick={saveFilme}>Salvar</button>
+            <button><a href={`https://youtube.com/results?search_query=${filme.title} trailer`} target="blank" rel="external">Trailer</a></button>
         </div>
+    </div>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import api from "../../services/api"
+import '../../index.css'
 import '../Home/home.css'
 // // URL da API: /movie/now_playing?api_key=da359e5e2f2da6874a02ea83beb3b3cf&language=pt-BR
 
@@ -23,6 +24,7 @@ export default function Home() {
             // ex: setFilmes(response.data.results.slice(0, 10))
             
             setFilmes(response.data.results)
+            setLoading(false)
         }
 
         loadFilmes()
@@ -41,7 +43,7 @@ export default function Home() {
                     {filmes.map((filme) => {
                         return(
                             <article key={filme.id}>
-                                <strong>{filme.title}</strong>
+                                <strong>{filme.title.toUpperCase()}</strong>
                                 <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
                                 <Link to={`/filme/${filme.id}`}>Acessar</Link>
                             </article>
